7464859f7e7036b6b3a05f8d8bc311111a772b83
Heureux115 <luuducanh11052005@gmail.com>
2025-06-08 12:38:27 +0700

    update

--- src/main/java/com/example/manager/controller/HomeController.java
+++ src/main/java/com/example/manager/controller/HomeController.java
@@ -1,19 +1,12 @@
 package com.example.manager.controller;
 
-import com.example.manager.entity.Employee;
 import com.example.manager.repository.EmployeeRepository;
 import com.example.manager.repository.UserRepository;
 import com.example.manager.service.EmployeeService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
 
 @Controller
 public class HomeController {
